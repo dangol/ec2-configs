dfcaddf6e19fe83b12ebc5e4847fbf4ecd6cdd10
Dan Goldberg <dan@madjic.net>
2018-08-14 17:19:49 -0400

    Fix patch to init files

--- launcher.sh
+++ launcher.sh
@@ -6,23 +6,23 @@ BOSTP=t2.medium
 RSTP=t2.medium
 NGHTTP=t2.large
 # mongo
-MONGOID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://~/github/sv-ansible/aws/mongo-config.txt --instance-type $MNGTP --subnet-id subnet-b12be39b --block-device-mappings file://mongomappings.json --count 1 --associate-public-ip-address)
+MONGOID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://mongo-config.txt --instance-type $MNGTP --subnet-id subnet-b12be39b --block-device-mappings file://mongomappings.json --count 1 --associate-public-ip-address)
 MONGOID=$(echo $ID | cut -f10 -d' ')
 aws ec2 create-tags --resources $THISID --tags "Key=env,Value=prod,Key=Name,Value=MongoDB"
 # web
-WEBID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://~/github/sv-ansible/aws/web-config.txt --instance-type $WEBTP --subnet-id subnet-b12be39b --block-device-mappings file://webmappings.json --count 1 --associate-public-ip-address)
+WEBID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://web-config.txt --instance-type $WEBTP --subnet-id subnet-b12be39b --block-device-mappings file://webmappings.json --count 1 --associate-public-ip-address)
 WEBID=$(echo $ID | cut -f10 -d' ')
 aws ec2 create-tags --resources $THISID --tags "Key=env,Value=prod,Key=Name,Value=Web"
 # bos
-BOSID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://~/github/sv-ansible/aws/bos-config.txt --instance-type $BOSTP --subnet-id subnet-b12be39b --block-device-mappings file://bosmappings.json --count 1 --associate-public-ip-address)
+BOSID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://bos-config.txt --instance-type $BOSTP --subnet-id subnet-b12be39b --block-device-mappings file://bosmappings.json --count 1 --associate-public-ip-address)
 WEBID=$(echo $ID | cut -f10 -d' ')
 aws ec2 create-tags --resources $THISID --tags "Key=env,Value=prod,Key=Name,Value=BOS"
 # RapidScreen
-RSID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://~/github/sv-ansible/aws/rs-config.txt --instance-type $RSTP --subnet-id subnet-b12be39b --block-device-mappings file://rsmappings.json --count 1 --associate-public-ip-address)
+RSID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://rs-config.txt --instance-type $RSTP --subnet-id subnet-b12be39b --block-device-mappings file://rsmappings.json --count 1 --associate-public-ip-address)
 RSID=$(echo $ID | cut -f10 -d' ')
 aws ec2 create-tags --resources $THISID --tags "Key=env,Value=prod,Key=Name,Value=RadidScreen"
 # Nightly
-NGHTID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://~/github/sv-ansible/aws/nightly-config.txt --instance-type $NGHTTP --subnet-id subnet-b12be39b --block-device-mappings file://nightlymappings.json --count 1 --associate-public-ip-address)
+NGHTID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://nightly-config.txt --instance-type $NGHTTP --subnet-id subnet-b12be39b --block-device-mappings file://nightlymappings.json --count 1 --associate-public-ip-address)
 RSID=$(echo $ID | cut -f10 -d' ')
 aws ec2 create-tags --resources $THISID --tags "Key=env,Value=prod,Key=Name,Value=Nightly"
 
