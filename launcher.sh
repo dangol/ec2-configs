594c209b1ea0da0ae1637288d313ad3871e70b1e
Dan <dan@madjic.net>
2018-08-15 11:02:39 -0400

    updates to launcher

--- launcher.sh
+++ launcher.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
-AMI=ami-9f086de0
-MNGTP=m5.2xlarge
+PRF="--profile sv"
+AMI=ami-456b493a
+MNGTP=t2.large
 WEBTP=t2.medium
 BOSTP=t2.medium
 RSTP=t2.medium
 NGHTTP=t2.large
-# mongo
-MONGOID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://mongo-config.txt --instance-type $MNGTP --subnet-id subnet-b12be39b --block-device-mappings file://mongomappings.json --count 1 --associate-public-ip-address)
+AWS_DEFAULT_REGION=us-east-1
+#mongo
+MONGOID=$(aws $PRF ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://mongo-config.txt --instance-type $MNGTP --subnet-id subnet-b12be39b --block-device-mappings file://mongomappings.json --count 1 --associate-public-ip-address)
 MONGOID=$(echo $ID | cut -f10 -d' ')
-aws ec2 create-tags --resources $THISID --tags "Key=env,Value=prod,Key=Name,Value=MongoDB"
+aws $PRF ec2 create-tags --resources $MONGOID --tags "Key=env,Value=prod,Key=Name,Value=MongoDB"
 # web
-WEBID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://web-config.txt --instance-type $WEBTP --subnet-id subnet-b12be39b --block-device-mappings file://webmappings.json --count 1 --associate-public-ip-address)
+WEBID=$(aws $PRF ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://web-config.txt --instance-type $WEBTP --subnet-id subnet-b12be39b --block-device-mappings file://webmappings.json --count 1 --associate-public-ip-address)
 WEBID=$(echo $ID | cut -f10 -d' ')
-aws ec2 create-tags --resources $THISID --tags "Key=env,Value=prod,Key=Name,Value=Web"
+aws $PRF ec2 create-tags --resources $WEBID --tags "Key=env,Value=prod,Key=Name,Value=Web"
 # bos
-BOSID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://bos-config.txt --instance-type $BOSTP --subnet-id subnet-b12be39b --block-device-mappings file://bosmappings.json --count 1 --associate-public-ip-address)
-WEBID=$(echo $ID | cut -f10 -d' ')
-aws ec2 create-tags --resources $THISID --tags "Key=env,Value=prod,Key=Name,Value=BOS"
+BOSID=$(aws $PRF ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://bos-config.txt --instance-type $BOSTP --subnet-id subnet-b12be39b --block-device-mappings file://bosmappings.json --count 1 --associate-public-ip-address)
+BOSID=$(echo $ID | cut -f10 -d' ')
+aws $PRF ec2 create-tags --resources $BOSID --tags "Key=env,Value=prod,Key=Name,Value=BOS"
 # RapidScreen
-RSID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://rs-config.txt --instance-type $RSTP --subnet-id subnet-b12be39b --block-device-mappings file://rsmappings.json --count 1 --associate-public-ip-address)
+RSID=$(aws $PRF ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://rs-config.txt --instance-type $RSTP --subnet-id subnet-b12be39b --block-device-mappings file://rsmappings.json --count 1 --associate-public-ip-address)
 RSID=$(echo $ID | cut -f10 -d' ')
-aws ec2 create-tags --resources $THISID --tags "Key=env,Value=prod,Key=Name,Value=RadidScreen"
+aws $PRF ec2 create-tags --resources $RSID --tags "Key=env,Value=prod,Key=Name,Value=RadidScreen"
 # Nightly
-NGHTID=$(aws ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://nightly-config.txt --instance-type $NGHTTP --subnet-id subnet-b12be39b --block-device-mappings file://nightlymappings.json --count 1 --associate-public-ip-address)
-RSID=$(echo $ID | cut -f10 -d' ')
-aws ec2 create-tags --resources $THISID --tags "Key=env,Value=prod,Key=Name,Value=Nightly"
+NGHTID=$(aws $PRF ec2 run-instances --image-id $AMI --key-name dbg-pub --security-group-ids sg-bf37d9c7  --user-data file://nightly-config.txt --instance-type $NGHTTP --subnet-id subnet-b12be39b --block-device-mappings file://nightlymappings.json --count 1 --associate-public-ip-address)
+NGHTID=$(echo $ID | cut -f10 -d' ')
+aws $PRF ec2 create-tags --resources $NGHTID --tags "Key=env,Value=prod,Key=Name,Value=Nightly"
 
 echo Mongo $MONGOID
 echo Web $WEBID
